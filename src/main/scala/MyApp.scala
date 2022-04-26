7dbbc75b20b1c4eb9a795ec057e447acbe81822c
Robert Kenny <robert.kenny@guardian.co.uk>
2022-04-26 14:56:38 +0100

    jiggle format

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,7 +1,11 @@
 package example.zio
 
-import example.zio.services._
+import services._
+
 import zio._
+
+// Reading list
+// - https://zio.dev/next/datatypes/
 
 object Main extends ZIOAppDefault {
   private val program =
@@ -9,12 +13,10 @@ object Main extends ZIOAppDefault {
       _ <- Console.print("Query: ")
       query <- Console.readLine
       results <- Capi.searchForContent(query)
-      _ <- ZIO.foreachDiscard(results)(result => Console.printLine(s"${result.webPublicationDate} - ${result.webTitle}"))
+      _ <- ZIO.foreachDiscard(results)(result =>
+        Console.printLine(s"${result.webTitle}\n${result.webUrl}\n"))
     } yield ()
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
     program.forever.provide(CapiLive.layer, ConfigLive.layer)
 }
-
-// Reading list
-// - https://zio.dev/next/datatypes/
